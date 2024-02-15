9390e6995497ab71db136e0706d55e55b4242a1e
Evan W <nfire11@gmail.com>
2024-02-15 20:55:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ DRIVER_PATH=$(pwd)
 
 cd ../metabase
 
-#DREMIO_DRIVER_VERSION=24.0.0-202302100528110223-3a169b7c
 DREMIO_DRIVER_VERSION=20.1.0-202202061055110045-36733c65
+
 clojure \
   -Sdeps '{:mvn/repos {"dremio-free" {:url "https://maven.dremio.com/free/"}} :aliases {:dremio { :extra-deps {com.dremio.distribution/dremio-jdbc-driver {:mvn/version "'${DREMIO_DRIVER_VERSION}'"} com.metabase/dremio-driver {:local/root "'${DRIVER_PATH}'"} }}}}'  \
   -X:build:dremio \
